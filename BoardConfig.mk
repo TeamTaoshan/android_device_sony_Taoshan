cee761795ba2f79b4cfa0cacd3b9da144aa9edfc
Shaaan <shantanulondhe26@gmail.com>
2013-08-03 06:59:29 +0100

    Various additions

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,9 +20,11 @@ BOARD_VOLD_MAX_PARTITIONS := 18
 
 # Platform
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := msm8230
+TARGET_BOARD_PLATFORM := msm8960
 BOARD_VENDOR_PLATFORM := taoshan
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 
+# Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
 BOARD_FLASH_BLOCK_SIZE := 131072
@@ -30,18 +32,54 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12828229120
 BOARD_CACHEIMAGE_PARTITION_SIZE := 576716800
 
+# Architecture
 TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_NEON := true
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+
+# Flags
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+
+# WiFi
+BOARD_HAS_QCOM_WLAN := true
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
+BOARD_WLAN_DEVICE := qcwcn
+
+# NFC
+BOARD_HAVE_NFC := true
 
 ## Kernel, bootloader
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_PREBUILT_KERNEL := device/sony/taoshan/kernel
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+
+# Graphics
+USE_OPENGL_RENDERER := true
+
+
+## EXPERIMENTAL
+BOARD_USES_QCOM_HARDWARE := true
+## Comment out if it renders builds unbootable.
 
 
 # Recovery
@@ -58,9 +96,6 @@ TARGET_OTA_ASSERT_DEVICE := C2104,taoshan
 TARGET_SPECIFIC_HEADER_PATH += device/sony/taoshan/include
 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/taoshan/bluetooth
-
-
-
 
 # inherit from the proprietary version
 -include vendor/sony/taoshan/BoardConfigVendor.mk
